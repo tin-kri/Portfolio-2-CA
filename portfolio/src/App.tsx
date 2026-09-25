1f485b5429cea802f1760b760bf2533f89e2dd42
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-09-25 08:28:11 +0200

    Adds dependencies

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section>
+       <h1>Hello there</h1>
+       <p>General Kenobi</p>
+         
+     </section>
+    </>
+  )
+}
+
+export default App
